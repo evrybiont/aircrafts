021f5c686d90ff05e2f02f808e6148809ee9e673
evrybiont <evrybiont@gmail.com>
2014-07-24 19:55:13 +0300

    Create menu buttons

--- app/heads/menu_state.h
+++ app/heads/menu_state.h
@@ -2,9 +2,14 @@
 #define MENU_STATE_H
 
 #include "game.h"
+#include <vector>
 
 class Menu: public State {
   SDL_Surface *background;
+  sdl::Button *btn;
+  std::vector<sdl::Button*> buttons;
+
+  void createButtons();
 
   public:
     Menu();
